f8da45e170cb220d087276fd9bd686499873cccb
Wendel Parrião <wendelparriao@yahoo.com.br>
2016-05-25 05:13:36 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/* Card.js plugin by Jesse Pollak. https://github.com/jessepollak/card */
+$('form').card({
+    container: '.card-wrapper',
+    width: 280,
+
+    formSelectors: {
+        nameInput: 'input[name="name-card"], input[name=""]'
+    }
+});
