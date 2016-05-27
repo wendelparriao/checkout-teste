a866079042bb7d9eaf49b11b6e0d93fb248ecefd
Wendel Parrião <wendelparriao@yahoo.com.br>
2016-05-27 01:04:38 -0300

    corrigindo bugs e colocando css de validação

--- js/index.js
+++ js/index.js
@@ -7,3 +7,31 @@ $('form').card({
         nameInput: 'input[name="name-card"], input[name=""]'
     }
 });
+
+function validateStep(step) {
+	var isValid = true;
+	if (step == undefined) {
+		// NOTE: porquê o formulário já inicia na etapa 2 ???
+		step = 2;
+	}
+
+	var fields = $('form .step-'+step+' [required]');
+
+	fields.each(function(key, f) {
+		var field = $(f);
+		if (field.val().length == 0 || (this.validity && !this.validity.valid)) {
+			isValid = false;
+			field.addClass('input-validate-error');
+		}else{
+			field.addClass('input-validate-true');
+		}
+	})
+
+	return isValid;
+}
+
+$('form').on('focusout keyup', 'input.input-validate-error', function() {
+	if ($(this).val().length > 0 && this.validity && this.validity.valid) {
+		$(this).removeClass('input-validate-error');
+	}
+});
